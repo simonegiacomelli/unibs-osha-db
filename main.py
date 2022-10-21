f3635d5f6ed6d2cfa8758a8c428e2a635e37a06a
Simone Giacomelli <simone.giacomelli@gmail.com>
2022-10-21 10:26:24 +0100

    better log.

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ from istanza_pagina import IstanzaPagina
 def main():
     pagina_size = 200
     istanza_index = 0
-    for office in estrai_codici_office():
-
+    codici_office = estrai_codici_office()
+    for office_index, office in enumerate(codici_office):
+        print(f'Codice office {office} {office_index + 1}/{len(codici_office)}')
         pagina = IstanzaPagina(office, istanza_index, pagina_size)
         while pagina is not None:
+            print('  ', end='')
             if not pagina.exists():
-                print(f'pagina {pagina} segue caricamento')
+                print(f'pagina {pagina} segue caricamento... ', end='')
                 pagina.load_from_url()
                 pagina.salva()
-                # pagina.salvaDatiIstanze()
             else:
-                print(f'pagina {pagina} gia'' presente')
+                pagina.load_from_disk()
+                print(f'pagina {pagina} gia'' presente ', end='')
 
+            print(f"`{pagina.catalogue.results_text}`")
             pagina = pagina.next()
 
 
--- osha/parser/instances_catalogue.py
+++ osha/parser/instances_catalogue.py
@@ -5,19 +5,21 @@ class InstanceCatalogue:
     def __init__(self, body: str):
         self.body = body
         self.instances_count = -1
+        self.results_text = 'na'
         self._body_changed()
 
     def _body_changed(self):
         soup = BeautifulSoup(self.body, 'html.parser')
 
-        # search = [f.find_all('div', class_='text-right') for f in soup.find_all('div', class_="span3")]
-        def filter(tag: Tag):
+        def results_filter(tag: Tag):
             return tag.name == 'div' and tag.get('class') == ['text-right'] and \
                    'Results' in tag.text
 
-        h = soup.find(filter)
+        h = soup.find(results_filter)
         if h is not None:
-            txt = h.text.split(' of ')[1]
+            self.results_text = h.text.strip()
+            txt = self.results_text.split(' of ')[1]
             self.instances_count = int(txt)
         else:
+            self.results_text = ''
             self.instances_count = -1
