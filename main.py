e063f154fc3d7025cad8e544b2ac352f8d544ff3
Simone Giacomelli <simone.giacomelli@lxc-xfce>
2022-10-22 18:51:12 +0200

    only blank office

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pathlib import Path
 
 from cache.source_cache import source_wrap
@@ -12,22 +13,17 @@ from osha.search_page import SearchPage, SearchData
 def main():
     pagina_size = 1000
     accident_index = 0
-    codici_office = [''] + estrai_codici_office()
 
-    for office_index, office in enumerate(codici_office):
-        office_ord = office_index + 1
-        office_len = len(codici_office)
-        search_page = SearchPage(office, accident_index, pagina_size)
-        while search_page is not None:
-            log(f'{office_ord:3}/{office_len:03} ', end='')
-            search_data: SearchData = source_wrap(search_page, search_page.cache_prefix).data()
+    search_page = SearchPage('', accident_index, pagina_size)
+    while search_page is not None:
+        search_data: SearchData = source_wrap(search_page, search_page.cache_prefix).data()
 
-            # if len(search_data.accident_detail_ids) > 0:
-            #     spp = str(search_page.cache_prefix) + '-detail'
-            #     detail_page = DetailPage(search_data.accident_detail_ids, Path(spp + '-page.html'))
-            #     source_wrap(detail_page, Path(spp)).data()
+        # if len(search_data.accident_detail_ids) > 0:
+        #     spp = str(search_page.cache_prefix) + '-detail'
+        #     detail_page = DetailPage(search_data.accident_detail_ids, Path(spp + '-page.html'))
+        #     source_wrap(detail_page, Path(spp)).data()
 
-            search_page = search_data.next_search_page()
+        search_page = search_data.next_search_page()
 
 
 if __name__ == '__main__':
